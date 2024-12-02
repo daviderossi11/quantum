2830e9bf7aaa4918bf6835e6bf67e19d35aecfd1
davide <rossidavide1102@gmail.com>
2024-12-02 15:19:14 +0100

    Refactor circuit metrics calculation for Gray Code; streamline theta calculation and circuit drawing

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from qiskit.circuit.library import HGate, CXGate, RYGate, XGate
 from numpy import arcsin, sqrt, pi
 import math
 from matplotlib import pyplot as plt
-# Funzione per calcolare il valore di θ
+
 def calculate_theta(value, max_value):
     X_N = value / sqrt(max_value)
     return 2 * arcsin(X_N)
@@ -25,62 +25,50 @@ def gray_code(n):
     
     return gray_codes
 
+# numero di qubit
+memory_values = [i for i in range(2**3)]
+N = len(memory_values)
+n  = int(math.log2(N))
 
-def calculate_circuit_metrics_GrayCode(memory_values):
-    max_value = sum([x**2 for x in memory_values])
-    thetas = [calculate_theta(value, max_value) for value in memory_values]
-    N = len(memory_values)
-    n = int(math.log2(N))
-    countXGate = 0
+max_value = sum([x**2 for x in memory_values])
 
-    qaddr = QuantumRegister(n, 'addr')
-    previous_bit = [1] * n
-    qdata = QuantumRegister(1, 'data')
-    circuit = QuantumCircuit(qaddr, qdata)
+qaddr = QuantumRegister(n, 'addr')
+previous_bit = [1] * n
+qdata = QuantumRegister(1, 'data')
+circuit = QuantumCircuit(qaddr, qdata)
     
-    for qa in qaddr:
-        circuit.append(HGate(), [qa])
-    circuit.barrier()
+for qa in qaddr:
+    circuit.append(HGate(), [qa])
+circuit.barrier()
     
-    gray_codes = gray_code(n)
-    binary_index = list(enumerate(gray_codes[0]))
+gray_codes = gray_code(n)
+binary_index = list(enumerate(gray_codes[0]))
     
-    for i, el in enumerate(memory_values):
-        theta = calculate_theta(el, max_value)
-        for j, bit in binary_index:
-            if bit == '0' and previous_bit[j] == 1:
+for i, el in enumerate(memory_values):
+
+    theta = calculate_theta(el, max_value)
+
+    for j, bit in binary_index:
+        if bit == '0' and previous_bit[j] == 1:
+            circuit.append(XGate(), [qaddr[j]])
+        previous_bit[j] = int(bit)
+    
+
+    CRYGate = RYGate(theta).control(n)
+    circuit.append(CRYGate, qaddr[:n] + [qdata[0]])
+        
+    if i < N - 1:
+        binary_index = list(enumerate(gray_codes[i + 1]))
+        for j, next_bit in binary_index:
+            if next_bit == '1' and previous_bit[j] == 0:
                 circuit.append(XGate(), [qaddr[j]])
-                countXGate+=1
-            previous_bit[j] = int(bit)
+    else:
+        for j, bit in binary_index:
+            if bit == '0':
+                circuit.append(XGate(), [qaddr[j]])
         
-        CRYGate = RYGate(theta).control(n)
-        circuit.append(CRYGate, qaddr[:n] + [qdata[0]])
-        
-        if i < N - 1:
-            binary_index = list(enumerate(gray_codes[i + 1]))
-            for j, next_bit in binary_index:
-                if next_bit == '1' and previous_bit[j] == 0:
-                    circuit.append(XGate(), [qaddr[j]])
-                    countXGate+=1
-        else:
-            for j, bit in binary_index:
-                if bit == '0':
-                    circuit.append(XGate(), [qaddr[j]])
-                    countXGate+=1
-        
-        circuit.barrier()
-
-        # Draw the circuit and display the number of X gates
-        circuit.draw(output='mpl')
-        plt.title(f'Circuit with {countXGate} X gates')
-        plt.show()
+    circuit.barrier()
 
 
-dataset_size = {i for i in range(1, 4)}
-
-for size in dataset_size:
-    memory_values = [i for i in range(1, (2**size) + 1)]
-
-    calculate_circuit_metrics_GrayCode(memory_values)
-
+circuit.draw('mpl', fold=30, scale=0.9)
 
