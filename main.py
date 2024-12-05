17f90e0758b4ade989e30d873392739de663cc85
davide <rossidavide1102@gmail.com>
2024-12-05 13:55:40 +0100

    Update circuit metrics visualization; change variable names for clarity and adjust plotting to reflect circuit size

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 from lib.circuit_metrics import ffqram_metrics_graycode, ffqram_metrics_classic
 from matplotlib import pyplot as plt
 
-size = range(1,8)
+size = range(1,7)
 
-classic_opt_depth = []
-gray_opt_depth = []
+classic_opt_size = []
+gray_opt_size = []
 
 for el in size:
-    _, classic_opt, _, _ = ffqram_metrics_classic(2**el)
-    _, gray_opt, _, _ = ffqram_metrics_graycode(2**el)
+    _, _, _, classic_opt = ffqram_metrics_classic(2**el)
+    _, _, _, gray_opt = ffqram_metrics_graycode(2**el)
     
-    classic_opt_depth.append(classic_opt)
+    classic_opt_size.append(classic_opt)
 
-    gray_opt_depth.append(gray_opt)
+    gray_opt_size.append(gray_opt)
 
 
+
+print(classic_opt_size)
+print(gray_opt_size)
 # plot del grafico della depth
 
 
@@ -22,9 +25,9 @@ plt.figure(figsize=(12, 6))
 
 # Tracciare Circuit Depth
 plt.subplot(1, 2, 1)
-plt.plot(size, classic_opt_depth, marker='o', color='yellow', linewidth=2, label='FF-QRAM Depth XGate')
-plt.plot(size, gray_opt_depth, marker='o', color='red', linewidth=2, label='FF-QRAM Depth GrayCode')
+plt.plot(size, classic_opt_size, marker='o', color='yellow', linewidth=2, label='FF-QRAM Depth XGate')
+plt.plot(size, gray_opt_size, marker='o', color='red', linewidth=2, label='FF-QRAM Depth GrayCode')
 plt.xlabel('Address Qubits')
-plt.ylabel('Circuit Depth')
+plt.ylabel('Circuit size')
 plt.legend()
 plt.title('opt_lvl=2') 
